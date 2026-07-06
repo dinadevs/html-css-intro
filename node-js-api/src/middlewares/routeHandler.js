19e54377453337737e388f98aa5d1bc11e509860
Edna Martins <ednamartins.work@gmail.com>
2026-07-06 08:45:25 -0300

    feat: get parameters

--- node-js-api/src/middlewares/routeHandler.js
+++ node-js-api/src/middlewares/routeHandler.js
@@ -2,12 +2,17 @@ import { routes } from "../routes.js";
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    return route.method === request.method && route.path === request.url;
+    return route.method === request.method && route.path.test(request.url);
   });
 
   if (route) {
-    return route.controller(request, response)
+    const routeParams = request.url.match(route.path);
+    const { ...params } = routeParams.groups;
+
+    request.params = params;
+    
+    return route.controller(request, response);
   }
-  
+
   return response.writeHead(404).end("Route not found!");
 }
--- node-js-api/src/routes.js
+++ node-js-api/src/routes.js
@@ -1,3 +1,5 @@
+import { parseRoutePath } from "./utils/parseRoutePath.js";
+
 export const routes = [
   {
     method: "GET",
@@ -17,7 +19,10 @@ export const routes = [
     method: "DELETE",
     path: "/products/:id",
     controller: (request, response) => {
-      return response.end("Removed");
+      return response.end("Product removed with ID: " + request.params.id);
     },
-  }
-];
+  },
+].map((route) => ({
+  ...route,
+  path: parseRoutePath(route.path),
+}));
